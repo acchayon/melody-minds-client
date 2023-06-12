42e5817d325b9e9d123da3822c1e43fe008f2483
acchayon <adhikaryac81@gmail.com>
2023-06-12 12:32:53 +0600

    implement dekete cart functionality

--- src/LayOuts/DashBoard.jsx
+++ src/LayOuts/DashBoard.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { FaCalendarTimes, FaHome, FaRestroom, FaShoppingCart, FaWallet } from 'react-icons/fa';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
+import UseCart from '../hooks/UseCart';
 
 const DashBoard = () => {
+    const [cart] = UseCart();
     return (
         <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
@@ -14,17 +16,22 @@ const DashBoard = () => {
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+                <ul className="menu p-4 w-80 h-full bg-neutral-300 text-base-content">
                     {/* Sidebar content here */}
-                    <li><Link><FaHome></FaHome> Student Home</Link></li>
-                    <li><Link><FaCalendarTimes></FaCalendarTimes> Reservation</Link></li>
-                    <li><Link><FaWallet></FaWallet> Payment History</Link></li>
-                    <li><Link to=''><FaShoppingCart></FaShoppingCart> Student Cart</Link></li>
+                    <li><NavLink to='/dashboard/home'><FaHome></FaHome> Student Home</NavLink></li>
+                    <li><NavLink to='/dashboard/reservation'><FaCalendarTimes></FaCalendarTimes> Reservation</NavLink></li>
+                    <li><NavLink to='/dashboard/payment'><FaWallet></FaWallet> Payment History</NavLink></li>
+                    <li>
+                        <NavLink to='/dashboard/mycart'>
+                            <FaShoppingCart></FaShoppingCart> 
+                            My Cart
+                            <span className="badge badge-secondary">+{cart?.length || 0}</span>
+                            </NavLink></li>
 
                     <div className="divider"></div>
 
-                    <li><Link to='/'><FaHome></FaHome> Home</Link></li>
-                    <li><Link to='/classes'><FaRestroom></FaRestroom> Classes</Link></li>
+                    <li><NavLink to='/'><FaHome></FaHome> Home</NavLink></li>
+                    <li><NavLink to='/classes'><FaRestroom></FaRestroom> Classes</NavLink></li>
 
                 </ul>
 
--- src/Pages/DashBoard/MyCart.jsx
+++ src/Pages/DashBoard/MyCart.jsx
@@ -1,9 +1,95 @@
 import React from 'react';
+import UseCart from '../../hooks/UseCart';
+import { FaTrashAlt } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const MyCart = () => {
+    const [cart, refetch] = UseCart();
+    const total = cart.reduce((sum, item) => item.price + sum, 0);
+
+
+    // delete
+    const handleDelete = item => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/carts/${item._id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+
+            }
+        })
+    }
+
+
     return (
-        <div>
-            <h2>My cart</h2>
+        <div className='w-full'>
+            <div className='flex justify-evenly text-2xl uppercase h-[30px] font-semibold'>
+                <h2>My cart : {cart.length}</h2>
+                <h2>My price: ${total}</h2>
+                <button className='btn btn-sm btn-neutral'>Pay</button>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr className='text-xl'>
+                            <th> #</th>
+                            <th>Image</th>
+                            <th>Class</th>
+                            <th>Instructor</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            cart.map((c, index) => <tr key={c._id}>
+                                <th>
+                                    {index + 1}
+                                </th>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={c.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {c.name}
+                                </td>
+                                <td>{c.instructor}</td>
+                                <td className='text-xl text-end'>${c.price}</td>
+                                <th>
+                                    <button onClick={() => handleDelete(c)} className="btn btn-ghost btn-lg"><FaTrashAlt></FaTrashAlt></button>
+                                </th>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
