6e47fce23232c991ce9ef312749cb88e7fd0c3b9
acchayon <adhikaryac81@gmail.com>
2023-06-13 19:56:28 +0600

    added dark/light toggle

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,18 +1,60 @@
-import React from 'react';
+// import React from 'react';
+// import Starter from '../Slider/Starter';
+// import Slider from '../Slider/Slider';
+// import Extra from '../Extra/Extra';
+// import PopularClass from '../PopularClass/PopularClass';
+
+// const Home = () => {
+//     return (
+//         <div>
+//             <div className='grid pt-40 grid-cols-1 md:grid-cols-2 items-center'>
+//                 <Starter></Starter>
+//                 <Slider></Slider>
+//             </div>
+//             <PopularClass></PopularClass>
+//             <Extra></Extra>
+//         </div>
+//     );
+// };
+
+// export default Home;
+
+
+
+
+
+
+import React, { useState } from 'react';
 import Starter from '../Slider/Starter';
 import Slider from '../Slider/Slider';
 import Extra from '../Extra/Extra';
 import PopularClass from '../PopularClass/PopularClass';
+import { FaLightbulb } from 'react-icons/fa';
 
 const Home = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleMode = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
+    const modeClass = isDarkMode ? 'bg-white text-black' : 'bg-slate-700 text-white';
+
     return (
-        <div>
-            <div className='grid pt-40 grid-cols-1 md:grid-cols-2 items-center'>
-                <Starter></Starter>
-                <Slider></Slider>
+        <div >
+
+            <div className={modeClass}>
+                <div className="grid pt-40 grid-cols-1 md:grid-cols-2 items-center">
+                    <Starter></Starter>
+                    <Slider></Slider>
+
+                </div>
+                <div className='pt-10 text-center'>
+                    <button className='btn ' onClick={toggleMode}> <FaLightbulb size={20}></FaLightbulb> </button>
+                </div>
+                <PopularClass></PopularClass>
+                <Extra></Extra>
             </div>
-            <PopularClass></PopularClass>
-            <Extra></Extra>
         </div>
     );
 };
--- src/Pages/Shared/Navber/Navbar.jsx
+++ src/Pages/Shared/Navber/Navbar.jsx
@@ -4,11 +4,15 @@ import { BsPersonCircle } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../Provider/AuthProvider';
 import UseCart from '../../../hooks/UseCart';
+import { useState } from 'react';
+
 
 
 const Navbar = () => {
     const { user, logOut } = useContext(AuthContext);
     const [cart] = UseCart();
+    
+
 
     const handleLogOut = () => {
         logOut()
@@ -27,42 +31,46 @@ const Navbar = () => {
             </button>
         </Link></li>
         {user && <li><Link to='/dashboard'>DashBoard</Link></li>}
+        
     </>
 
     return (
-        <div>
-            <div className="navbar bg-neutral-400 max-w-screen-xl fixed z-10 bg-opacity-30">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu text-xl menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        
+            <div>
+                <div className="navbar bg-neutral-400 max-w-screen-xl fixed z-10 bg-opacity-30">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </label>
+                            <ul tabIndex={0} className="menu text-xl menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                                {navMenu}
+                            </ul>
+                        </div>
+                        <img className='w-2/12 rounded-full' src="https://i.ibb.co/sQ40KKv/melody.png" alt="" />
+                        <a className="btn btn-ghost normal-case font-extrabold md:text-3xl">Melody Minds</a>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu text-xl menu-horizontal px-1">
                             {navMenu}
                         </ul>
                     </div>
-                    <img className='w-2/12 rounded-full' src="https://i.ibb.co/sQ40KKv/melody.png" alt="" />
-                    <a className="btn btn-ghost normal-case font-extrabold md:text-3xl">Melody Minds</a>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu text-xl menu-horizontal px-1">
-                        {navMenu}
-                    </ul>
-                </div>
-                <div className="navbar-end ">
-                    {user ? <>
-                        <button onClick={handleLogOut} className="btn btn-sm btn-active btn-neutral">LogOut</button>
-                    </> : <>
-                        <Link to='/login' className="btn btn-active mr-4 btn-sm btn-neutral">Login <FaArrowRight></FaArrowRight> </Link>
-                    </>}
-                    <div className="avatar">
-                        <div className="w-16 rounded">
-                            <BsPersonCircle className='mt-3' size={40}></BsPersonCircle>
+                    <div className="navbar-end ">
+                        {user ? <>
+                            <button onClick={handleLogOut} className="btn btn-sm btn-active btn-neutral">LogOut</button>
+                        </> : <>
+                            <Link to='/login' className="btn btn-active mr-4 btn-sm btn-neutral">Login <FaArrowRight></FaArrowRight> </Link>
+                        </>}
+                        <div className="avatar">
+                            <div className="w-16 rounded">
+                                <BsPersonCircle className='mt-3' size={40}></BsPersonCircle>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        
+
     );
 };
 
